fa2efc054113d0472240e7ed2fdb479352dd8e78
anfark <roman-schulte@gmx.de>
2018-02-20 21:42:45 +0100

    updated gitignore

--- source/conf.py
+++ source/conf.py
@@ -41,6 +41,7 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
+    'javasphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
